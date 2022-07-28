1492d4e4d5d6197e09aa129a4e0021d594e3bb65
kdmstj <rkddmstj429@gmail.com>
2022-07-28 15:54:29 +0900

    first commit

---
+++ src/main/java/seoham/seohamspring/SeohamSpringApplication.java
@@ -0,0 +1,13 @@
+package seoham.seohamspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeohamSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeohamSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/seoham/seohamspring/SeohamSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package seoham.seohamspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeohamSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
