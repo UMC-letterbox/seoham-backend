99a52aba817abf6e331e51c3a7c280df81ae3d21
kdmstj <rkddmstj429@gmail.com>
2022-08-19 15:55:35 +0900

    config swagger3.0

--- src/main/java/seoham/seohamspring/SeohamSpringApplication.java
+++ src/main/java/seoham/seohamspring/SeohamSpringApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 //@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @SpringBootApplication
+@EnableWebMvc
 public class SeohamSpringApplication {
 
 	public static void main(String[] args) {
