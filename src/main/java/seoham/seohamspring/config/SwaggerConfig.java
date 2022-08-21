141f99dc7b141e1b1e18cee2231d4e68c262a14c
kdmstj <rkddmstj429@gmail.com>
2022-08-21 19:30:33 +0900

    s

--- src/main/java/seoham/seohamspring/config/SwaggerConfig.java
+++ src/main/java/seoham/seohamspring/config/SwaggerConfig.java
@@ -10,8 +10,8 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
-@Configuration
 @EnableWebMvc
+//@Configuration
 public class SwaggerConfig {
 
     @Bean
