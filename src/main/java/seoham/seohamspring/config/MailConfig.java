6907c1d537171b00efd502a140003ced1b0bc30f
rjh71 <rjh7100@gmail.com>
2022-11-06 20:58:52 +0900

    feat: 메일 전송 기능

--- src/main/java/seoham/seohamspring/config/MailConfig.java
+++ src/main/java/seoham/seohamspring/config/MailConfig.java
@@ -1,41 +1,41 @@
-package seoham.seohamspring.config;
-
-import java.util.Properties;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-
-@Configuration
-public class MailConfig {
-    
-    @Bean
-    public JavaMailSender javaMailService() {
-        JavaMailSender javaMailSender = new JavaMailSenderImpl();
-
-        ((JavaMailSenderImpl) javaMailSender).setHost("smtp.naver.com");
-        ((JavaMailSenderImpl) javaMailSender).setUsername("seoham2022@naver.com");
-        ((JavaMailSenderImpl) javaMailSender).setPassword("seoham2022!");
-
-        ((JavaMailSenderImpl) javaMailSender).setPort(465);
-
-        ((JavaMailSenderImpl) javaMailSender).setJavaMailProperties(getMailProperties());
-
-        return javaMailSender;
-        
-    }
-
-    private Properties getMailProperties() {
-        Properties properties = new Properties();
-        properties.setProperty("mail.transport.protocol", "smtp");
-        properties.setProperty("mail.smtp.auth", "true");
-        properties.setProperty("mail.smtp.starttls.enable", "true");
-        properties.setProperty("mail.debug", "true");
-        properties.setProperty("mail.smtp.ssl.trust", "smtp.naver.com");
-        properties.setProperty("mail.smtp.ssl.enable", "true");
-
-        return properties;
-    }
-    
-}
+//package seoham.seohamspring.config;
+//
+//import java.util.Properties;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.mail.javamail.JavaMailSenderImpl;
+//
+//@Configuration
+//public class MailConfig {
+//
+//    @Bean
+//    public JavaMailSender javaMailService() {
+//        JavaMailSender javaMailSender = new JavaMailSenderImpl();
+//
+//        ((JavaMailSenderImpl) javaMailSender).setHost("smtp.naver.com");
+//        ((JavaMailSenderImpl) javaMailSender).setUsername("seoham2022@naver.com");
+//        ((JavaMailSenderImpl) javaMailSender).setPassword("seoham2022!");
+//
+//        ((JavaMailSenderImpl) javaMailSender).setPort(465);
+//
+//        ((JavaMailSenderImpl) javaMailSender).setJavaMailProperties(getMailProperties());
+//
+//        return javaMailSender;
+//
+//    }
+//
+//    private Properties getMailProperties() {
+//        Properties properties = new Properties();
+//        properties.setProperty("mail.transport.protocol", "smtp");
+//        properties.setProperty("mail.smtp.auth", "true");
+//        properties.setProperty("mail.smtp.starttls.enable", "true");
+//        properties.setProperty("mail.debug", "true");
+//        properties.setProperty("mail.smtp.ssl.trust", "smtp.naver.com");
+//        properties.setProperty("mail.smtp.ssl.enable", "true");
+//
+//        return properties;
+//    }
+//
+//}
--- src/main/java/seoham/seohamspring/mail/MailController.java
+++ src/main/java/seoham/seohamspring/mail/MailController.java
@@ -1,4 +1,33 @@
 package seoham.seohamspring.mail;
 
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import seoham.seohamspring.config.BaseResponse;
+
+import javax.mail.MessagingException;
+import java.io.UnsupportedEncodingException;
+
+@Controller
+@RequiredArgsConstructor
+@Api(tags = "mail")
+@RequestMapping("/mail")
 public class MailController {
+
+
+    private final MailServcie mailServcie;
+
+    @ResponseBody
+    @PostMapping("/send")
+    public String mailConfirm(@RequestBody EmailAuthRequestDto emailDto) throws MessagingException, UnsupportedEncodingException {
+
+        String authCode = mailServcie.sendEmail(emailDto.getEmail());
+        return authCode;
+    }
+
 }
